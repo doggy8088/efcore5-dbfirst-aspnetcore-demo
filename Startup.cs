d88ed5291b6c76c6f1e6e75359bd05fe774f0d9f
Will 保哥 <doggy.huang@gmail.com>
2021-10-08 19:43:55 +0800

    將 ContosoUniversity 加入 ServiceCollection

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using efcore5_dbfirst_aspnetcore_demo.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,13 @@ namespace efcore5_dbfirst_aspnetcore_demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContosoUniversityContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                options.EnableSensitiveDataLogging();
+
+                options.LogTo(Console.WriteLine, LogLevel.Information);
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
