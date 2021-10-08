412fa0c73820d8ebcc1d5bb2552bcb817c6ea0bf
Will 保哥 <doggy.huang@gmail.com>
2021-10-08 19:25:00 +0800

    使用 dotnet ef dbcontext scaffold 加入 EFCore 實體資料模型
    dotnet ef dbcontext scaffold "Server=(localdb)\Projects;Database=ContosoUniversity;Trusted_Connection=True;MultipleActiveResultSets=true" Microsoft.EntityFrameworkCore.SqlServer -o Models -f

---
+++ Models/OfficeAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace efcore5_dbfirst_aspnetcore_demo.Models
+{
+    public partial class OfficeAssignment
+    {
+        public int InstructorId { get; set; }
+        public string Location { get; set; }
+
+        public virtual Person Instructor { get; set; }
+    }
+}
